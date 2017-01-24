248801a99dd8f754e40acf6cff8a5bb91af1c4c0
Eric <EricS@Erics-Air.domain>
2017-01-23 20:07:39 -0700

    Ajax jquery add number of likes and refresh them on page

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,15 +1,32 @@
 $(document).ready(function() {
 
-	console.log("Hello World")
 	$(".not-liked").click(function() {
+		console.log("Hello World")
+		// div has data-(name you make up) to identify data 
+		// inside element and bring up inside jquery
+		
+		var that = this;
+		var myPostId = $(this).data('post-id');
+		console.log(myPostId)
 
-		$(this).css("background-color" , "gray")
 		$.post(
 			"/likes", 
-			{ post_id: $(this).data('post-id'), soda: "sprite" },
+			{ post_id: myPostId, soda: "sprite" },
 			function(data) {
-				console.log(data)
+				$(that).css("background-color" , "gray");
+				// get the value
+				var numlikes = $(".number-likes-"+myPostId).text() ;
+
+				// convert it to a number
+				// add one and put it back
+				$(".number-likes-"+myPostId).text(parseInt(numlikes) + 1);
+
+
 			}
 		)
+
+
+
 	})
+
 })
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
 
   mount_uploader :attachment, AttachmentUploader
 has_many :users, through: :likes
+has_many :likes 
 end
